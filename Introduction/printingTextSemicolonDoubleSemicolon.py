e43cc46892e9067b9d7f195bc77e990b00dbbfe0
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-16 13:44:13 +0600

    #

---
+++ Introduction/printingTextSemicolonDoubleSemicolon.py
@@ -0,0 +1,7 @@
+print("My favorite quote is \"To be or not to be.\"")
+# this is how you handel double quotation inside another double quote 
+
+
+# single quote solution 
+
+print('My favourite quote is "To be or not to be "')
