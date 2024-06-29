2fdd2495749d611f0d8782577d6c085ee759d933
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 04:21:45 +0600

    short hand operator.py

---
+++ Operators/shortHandOperator.py
@@ -0,0 +1,18 @@
+count = 0
+
+
+# -= is shorthand for count = count - 
+# += is shorthand for count = count +
+# /= is shorthand for count = count / 
+# %= is shorthand for count = count % 
+# //= is shorthand for count = count // (// this is floor division)
+
+# **= is shorthand for count = count **
+
+
+count +=5
+print(count)
+count -=2
+print(count)
+count +=count
+print(count)
