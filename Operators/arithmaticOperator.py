cdd6fbde11a453f6a2e5eb36f7ce7e60b292c015
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 03:50:44 +0600

    #

---
+++ Operators/arithmaticOperator.py
@@ -0,0 +1,18 @@
+a , b , c  = 2, 2, 0.5
+
+totalSum = a+b+c
+
+totalProduct = a*b*c
+
+print(totalSum)
+print(0-totalSum)
+print(totalProduct)
+print(totalSum/totalProduct)
+
+
+# everytime interger + interger = integer but sometimes, 
+
+
+#every time all the operator here give or return interger value if both values are integer but only division here give the float 
+
+ 
