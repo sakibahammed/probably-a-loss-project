48321cea7e01118fbc8f80141f162452eccd3213
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 04:12:18 +0600

    more operator

---
+++ Operators/moreOperator.py
@@ -0,0 +1,22 @@
+# floor division operator (//) == its simply like division but it returns us the integer one and down it. like if we divide , 5/2 = 2.5 .. but the floor division makes it like this ,  5//2 = 2
+
+
+# exponent operator (**) its like the power
+
+
+# mod operator(#) its the modulus operator
+
+
+a , b , c = 2,8,5
+
+product_a_b = a*b
+print(product_a_b)
+
+
+print(product_a_b // c)
+print(a ** b)
+print(b%c)
+ 
+
+
+
