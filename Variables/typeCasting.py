05ee9bc89f8030875d6f4e4c3651e602f7b1f7e8
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 03:35:18 +0600

    fun by typecasting

---
+++ Variables/typeCasting.py
@@ -0,0 +1,30 @@
+#type casting in python 
+
+
+
+piNumbers = 3.14152
+
+rootOf8 = 2.19
+print(int(piNumbers))
+
+print(int(rootOf8))
+
+
+# we cant convert everything in integer like , 
+
+
+number = 'hello mom'
+
+
+# print(int(number))
+
+# but if we do this
+
+
+number = '2'
+
+print((type(number)))
+
+number = int(number)
+print(type(number))
+
