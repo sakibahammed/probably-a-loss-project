a1748610ea0429026730ae48416ad9a6cce1e510
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 02:55:02 +0600

    variable type

---
+++ Variables/variableTypes.py
@@ -0,0 +1,13 @@
+integer_type = 25
+float_type = 74.2
+is_true = False
+name = 'sakib'
+peopleWhoLikeMe = []
+
+
+
+print(integer_type , type(integer_type))
+print(float_type , type(float_type))
+print(is_true , type(is_true))
+print(name , type(name))
+print(peopleWhoLikeMe, type(peopleWhoLikeMe))
