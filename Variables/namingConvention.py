55c92ce4182c903a8e41680b9c88925e97a06e53
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 02:30:20 +0600

    naming convention

---
+++ Variables/namingConvention.py
@@ -0,0 +1,22 @@
+# you can name your variable in any convention. Its not the end of the world . 
+
+# we can name the variable using camel case , or snake_case or Pascal Case
+
+
+
+
+# camel Case : 
+
+
+myName = 'sakib'
+
+
+# snake_case : 
+
+her_name = 'sadia'
+
+# pascal case : 
+
+
+HelloMom = 'hi mom'
+
