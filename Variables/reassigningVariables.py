541758b1376c4605c4bed481ea58769790d0560d
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 02:35:39 +0600

    reassignment in python

---
+++ Variables/reassigningVariables.py
@@ -0,0 +1,10 @@
+# two variables cannt have same name , it will do the reassigning
+
+message = "the first message"
+
+print(message)
+message = "the second message"
+print(message)
+
+message = "this is third message"
+print(message)
