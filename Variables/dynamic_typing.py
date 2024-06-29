be3dac7f4be0fc1b27d56e2153777a6d62fd450f
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 03:00:22 +0600

    dynamic type python

---
+++ Variables/dynamic_typing.py
@@ -0,0 +1,6 @@
+# the type of the variable can change in any time no matter what.. its possible because of its , dynamically changing feature.. but in java its not possible.
+
+variable = 10
+print(variable , type(variable))
+variable = 'what the heck is this'
+print(variable , type(variable))
