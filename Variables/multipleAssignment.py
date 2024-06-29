e1b644268284cb2a75df04d05eada729f9603f0e
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 02:47:28 +0600

    multiple-assignment complete

---
+++ Variables/multipleAssignment.py
@@ -0,0 +1,21 @@
+msg1 , msg2 = 'world' , 'hello'
+
+msg3 , msg4 , msg5  = 'name' , 'is' , 'my'
+
+msg6 = 'sakib'
+# don't change code there
+
+# fix this bug using multiple line assignment.
+
+msg2 ,msg1 = msg1 , msg2
+
+msg3 , msg4 , msg5 = msg5 , msg4 , msg3
+
+# don't change code here
+
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg4)
+print(msg5)
+print(msg6)
