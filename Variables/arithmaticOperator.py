171ebf07e242619f300702b3cfb82aef9d5c729c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-06-30 03:49:58 +0600

    #

--- Variables/arithmaticOperator.py
+++ Variables/arithmaticOperator.py
@@ -8,3 +8,11 @@ print(totalSum)
 print(0-totalSum)
 print(totalProduct)
 print(totalSum/totalProduct)
+
+
+# everytime interger + interger = integer but sometimes, 
+
+
+#every time all the operator here give or return interger value if both values are integer but only division here give the float 
+
+ 
