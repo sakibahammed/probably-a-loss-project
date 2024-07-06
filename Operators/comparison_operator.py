2e611eaacd2e9552ad8cd3245b2510486f279394
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-07-07 02:30:02 +0600

    comparison operator

---
+++ Operators/comparison_operator.py
@@ -0,0 +1,21 @@
+def checkEqual(x,y) -> bool:
+    return x==y 
+
+def check_not_equal(x ,y):
+    return x!=y
+
+def check_less_than_orEqual(x,y):
+    return x<=y
+
+def check_greater_than_or_equal(x,y):
+    return x>=y
+
+def check_less(x,y):
+    return x<y
+
+
+
+
+
+equalOrNotEqual = check_not_equal(1,2)
+print(equalOrNotEqual)
