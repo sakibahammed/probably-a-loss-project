a2b6d411a9f96822003a191ac3ccb7304033709c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-08-11 16:00:05 +0600

    condition

---
+++ condition/condition.py
@@ -0,0 +1,12 @@
+def is_balance_low(balance: int)-> None:
+    if balance<= 100:
+        return "balance is low"
+    else : return "not low"
+
+print(is_balance_low(2000))
+        
+
+# if statment does not create any new scope
+
+
+    
