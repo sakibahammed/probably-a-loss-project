e362477691418443b97b6c4ee010f6ab239f1370
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-08-12 02:59:00 +0600

    for loop step up down

--- loops/forLoopStart.py
+++ loops/forLoopStart.py
@@ -1,7 +1,11 @@
-for i in range(5,19):
-    print(i)
+# for i in range(5,19):
+    # print(i)
     
 # for loop step
 
-for i in range(0,110,10):
-    print(i)
+# for i in range(0,110,10):
+    # print(i)
+    
+    
+for k in range(20,9,-1):
+    print(k)
