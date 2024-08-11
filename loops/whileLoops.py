599b7701196ad842198388a2c29ce61545cc7774
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-08-12 01:13:33 +0600

    while loops

--- loops/whileLoops.py
+++ loops/whileLoops.py
@@ -1,4 +1,22 @@
-i=0
-while i <5:
-    print(i)
-    i = i+1
+# i=0
+# while i <5:
+    # print(i)
+    # i +=1
+    
+    
+# print to 0 - 9 
+
+
+# n = 0 
+
+# while n<=9:
+#     print(n)
+#     n+=1
+
+
+# m = 1
+
+# while m<=9:
+#     m=m*10
+#     print(m)
+#     m+= 1
