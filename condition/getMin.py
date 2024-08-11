7503f5cb615037e6b644294600bb646456b7bffc
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-08-11 17:11:42 +0600

    if elif else

--- condition/getMin.py
+++ condition/getMin.py
@@ -1,8 +1,9 @@
 def get_min(a:int , b:int)-> int:
     if a<b :
         return a
-    
+    elif a==b: 
+        return "equal"
     else :  return b
     
     
-print(get_min(40,7))
+print(get_min(40,40))
