835e3531b8f3660c22e93e4bdeaf42b4a77b1b56
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-08-16 01:44:59 +0600

    len check

---
+++ length-function.py/return_longer_function.py
@@ -0,0 +1,17 @@
+def get_longer_word(word1:str , word2:str)->str:
+    if len(word1) > len(word2):
+        return word1
+    
+    elif len(word1) == len(word2):
+        return "both are same"
+    
+    else:
+        return word2
+        
+    
+
+
+
+
+
+print(get_longer_word('yellow' , 'yellow'))
